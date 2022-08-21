d3a608dfa86a36c10b1b54a5de6c7493aed3a9d1
sajederanji <111282959+sajederanji@users.noreply.github.com>
2022-08-21 14:06:59 +0430

    Add files via upload

---
+++ vs ranji/ostovaneh.py
@@ -0,0 +1,9 @@
+shoae = float(input('give me shoae:'))
+ertefa = float(input('give me ertefa:'))
+pi = 3.14
+masahat_janebi = (2*pi*shoae*ertefa)
+masahat_kol = (masahat_janebi+2*pi*shoae*shoae)
+hajm = (pi*shoae*shoae*ertefa)
+print('masahat_janebi_ostovane:' ,masahat_janebi)
+print('masahat_kol_ostovane:' ,masahat_kol)
+print('hajm_ostovane:' ,hajm)
---
+++ vs ranji/triangle.py
@@ -0,0 +1,4 @@
+ghaede = float(input("please enter your ghaede: "))
+ertefa = float(input("please enter your ertefa: "))
+masahat = (ghaede * ertefa)/2
+print("triangle_area:" ,masahat)
